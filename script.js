6f65cf5017fdcfda01bfc2ba4e90ae80320bc04f
Frost1x <Anthonyq4032@gmail.com>
2026-08-14 22:14:49 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function setAbout(){
+  const aboutSection = document.getElementById("aboutMeFont");
+  aboutSection.scrollIntoView({behavior: "smooth", block: "start"});
+}
+function setSkills(){
+  const skillsSection = document.getElementById("skillsFont");
+  skillsSection.scrollIntoView({behavior: "smooth", block: "start"});
+}
+function setEducation(){
+  const educationSection = document.getElementById("educationFont");
+  educationSection.scrollIntoView({behavior: "smooth", block: "start"});
+}
+
+function scrollProject(){
+  const element = document.getElementById("projectFont");
+  element.scrollIntoView({behavior: "smooth", block: "start"});
+}
